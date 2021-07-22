37506e4d5d921bb7400830cb7f11740807a6da09
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-22 13:39:57 -0500

    updated routes for photos

---
+++ migrations/20210722162100_create_live.js
@@ -0,0 +1,20 @@
+
+  // exports.up = function(knex) {
+  //   return knex.schema.createTable('live', table => {
+  //     table.increments('id'); // adds an auto incrementing PK column
+  //     table.string('title').notNullable();+
+  //     table.string('category');
+  //     table.string('description', 2000);
+  //     table.string('metadata', 2000);
+  //     table.string('image_url', 1000)
+  //   });
+  // };
+
+  // exports.down = function(knex) {
+  //   return knex.schema.dropTableIfExists('live');
+  // };
+
+
+
+
+
--- routes/photos_main.js
+++ routes/photos_main.js
@@ -31,6 +31,36 @@ router.get('/nature', function (req, res) {
   console.log(`GET submitted`)
 })
 
+router.get('/urban', function (req, res) {
+  knex
+    .select('*')
+    .from('photos')
+    .where({category: 'urban'})
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'Not connecting to DB'
+      })
+    );
+  console.log(`GET submitted`)
+})
+
+router.get('/live', function (req, res) {
+  knex
+    .select('*')
+    .from('photos')
+    .where({category: 'live'})
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'Not connecting to DB'
+      })
+    );
+  console.log(`GET submitted`)
+})
+
 
 
 // router.post('/', function (req, res) {
