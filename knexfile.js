a67e900174df22dbc95968fc610a94c45e8e94e3
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-23 11:46:17 -0500

    update scripts, env settings

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,18 @@
-// Update with your config settings.
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://postgres:docker@localhost/trw'
+    connection: process.env.DB_CONNECTION_STRING || 'postgres://postgres:docker@localhost/trw',
     // replace USER_NAME, USER_PASSWORD, and DATABASE_NAME with your Docker PostgreSQL container's username, password and an *empty* database
     // that you have created on your Docker PostgreSQL container volume
+    migrations: {
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
+    },
   },
 
   staging: {
@@ -26,11 +32,9 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      connectionString
     },
     pool: {
       min: 2,
