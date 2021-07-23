bcc7459ab771e07b6c8da3f28c8a02d7b6b887f8
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-23 13:40:09 -0500

    update start scripts

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors())
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,21 +30,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/photos', photosRouter);
 app.use('/users', usersRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// development error handler will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      'dev error': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    'prod error': {
+      message: err.message,
+      error: {}
+    }
+  });
 });
-
 module.exports = app;
