97579c36ce11b38b4a320dad00ed63884f715022
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-23 14:26:42 -0500

    change prod env settings

--- knexfile.js
+++ knexfile.js
@@ -18,13 +18,15 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://derlasgqvqqhrd:abec1b09723f6ec047fbea80784de1aa2c7fa349cf4b870a6b007419daa483ff@ec2-18-206-20-102.compute-1.amazonaws.com:5432/d20hod2kh31e03',
+    connection: {
+      connectionString,
       ssl : {
         rejectUnauthorized:false
-      },
-
-    migrations: { directory: __dirname + "/migrations"},
-    seeds: { directory: __dirname + "/seeds"},
-}
+      }
+    },
+    migrations: {
+      tableName: 'knex_migrations_photos'
+    }
+  }
 
 };
